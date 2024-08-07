f7da6e9a8a29cfc4d674229bd0648eaee779650f
Dalufishe <fishawe87@gmail.com>
2024-08-07 13:54:57 +0800

    add error handler

--- vcgrab.js
+++ vcgrab.js
@@ -18,40 +18,54 @@ async function main() {
 main();
 
 async function fetchNode(url) {
+
+  sleep(100)
+
   var headers = new Headers({
     Authorization: `Basic ${btoa(USERNAME + ":" + PASSWORD)}`,
   });
 
-  const res = await fetch(url, { headers: headers });
+  try {
+    const res = await fetch(url, { headers: headers });
 
-  // download files
+    // download files
 
-  const node = url.split("/").slice(-1)[0];
+    const node = url.split("/").slice(-1)[0];
 
-  if (node?.includes(".")) {
-    const buffer = await res.arrayBuffer();
+    if (node?.includes(".")) {
+      const buffer = await res.arrayBuffer();
 
-    await fs.mkdir(path.join(url.slice(23, -node.length)), { recursive: true });
+      await fs.mkdir(path.join(url.slice(23, -node.length)), {
+        recursive: true,
+      });
 
-    fs.writeFile(
-      path.join(url.slice(23, -node.length), node),
-      Buffer.from(buffer)
-    );
-    return;
+      fs.writeFile(
+        path.join(url.slice(23, -node.length), node),
+        Buffer.from(buffer)
+      );
+    }
+    const html = await res.text();
+    const $ = cheerio.load(html);
+
+    const hrefs = $(".link a")
+      .slice(1)
+      .map(function () {
+        // console.log($(this).attr("href"));
+        return $(this).attr("href");
+      })
+      .toArray();
+
+    hrefs.forEach((href) => {
+      fetchNode(url + "/" + href);
+    });
+  } catch (e) {
+    console.log("Grabbing Failed: " + e.message);
   }
+}
 
-  const html = await res.text();
-  const $ = cheerio.load(html);
 
-  const hrefs = $(".link a")
-    .slice(1)
-    .map(function () {
-      // console.log($(this).attr("href"));
-      return $(this).attr("href");
-    })
-    .toArray();
-
-  hrefs.forEach((href) => {
-    fetchNode(url + "/" + href);
+function sleep(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
   });
 }
